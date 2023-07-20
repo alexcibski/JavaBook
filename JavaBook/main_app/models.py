36e889fa5a293d108828d0cf5b238fc2244d9f92
alcib-22 <ciborowski.alex@gmail.com>
2023-07-20 15:23:24 -0700

    added dropdowns to coffee and maker forms

--- JavaBook/main_app/models.py
+++ JavaBook/main_app/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 from django.urls import reverse
 
+RATE = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5')
+)
+ROAST = (
+    ('L', 'Light'),
+    ('M', 'Medium'),
+    ('D', 'Dark')
+)
+
 class Coffee(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    roast = models.CharField(max_length=100, blank=True)
+    roast = models.CharField(
+        'Roast',
+        max_length=1,
+        choices = ROAST
+    )
     countryOfOrigin = models.CharField(max_length=100, blank=True)
     tastingNotes = models.CharField(max_length=100, blank=True)
-    rating = models.PositiveIntegerField()
+    rating = models.CharField(
+        'Rating',
+        max_length=1,
+        choices = RATE
+    )
     review = models.TextField(max_length=250, blank=True)
     brewTips = models.TextField(max_length=250, blank=True)
     image = models.URLField(max_length=300, blank=True)
@@ -21,7 +42,11 @@ class Coffee(models.Model):
 class Maker(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    rating = models.PositiveIntegerField()
+    rating = models.CharField(
+        'Rating',
+        max_length=1,
+        choices = RATE
+    )
     description = models.TextField(blank=True)
     review = models.TextField(blank=True)
     brewTips = models.TextField(blank=True)
